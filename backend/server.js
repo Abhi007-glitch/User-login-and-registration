adf6a9ecccee6f560191902b42a2464f90713ed6
Abhi007-glitch <abhinav619000@gmail.com>
2022-02-16 20:47:16 +0530

    first

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+
+
+dotenv.config();
+app.get('/', (req, res) => {
+    res.send("API is working");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`server started on port ${PORT}`));
