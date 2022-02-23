4fc6e0674ed9170711a424b239fbcb68f43a3a9a
Abhi007-glitch <abhinav619000@gmail.com>
2022-02-23 19:20:41 +0530

    proxy-resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,16 @@ import Footer from './components/Footer/Footer';
 import LandingPage from './Screen/LandingPage/LandingPage';
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Registeration from './Screen/Registeration/Registeration';
+import RegisterScreen from './Screen/RegisterScreen/RegisterScreen';
+import LoginScreen from './Screen/LoginScreen/LoginScreen';
 function App() {
   return (
     <BrowserRouter>
       <main>
         <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        <Route path="/register" element={<Registeration/>} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginScreen/>}/>
+        <Route path="/register" element={<RegisterScreen/>} />
         </Routes>
       </main>
       <Footer />
---
+++ frontend/src/Screen/RegisterScreen/RegisterScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MainScreen from '../../components/MainScreen'
+
+const RegisterScreen = () => {
+  return (
+      <MainScreen title='REGISTERATION'>Registeration</MainScreen>
+  )
+}
+
+export default RegisterScreen
